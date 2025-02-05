212f267ac7aaa23ccef68dcddf4f82dd776ff392
Alexandr <blix@list.ru>
2025-02-05 19:09:37 +0300

    ci test 3.15

--- tracker/models.py
+++ tracker/models.py
@@ -17,7 +17,7 @@ class Habits(models.Model):
         User, on_delete=models.CASCADE, **NULLABLE, verbose_name="Создатель"
     )
     place = models.CharField(max_length=100, verbose_name="Место")
-    time = time = models.TimeField(
+    time = models.TimeField(
         default=timezone.now().time(), verbose_name="Время выполнения"
     )
     action = models.CharField(max_length=100, verbose_name="Действие")
